0a0fb73cdbb459d8781bf7c0ca61d1984143329e
aHazeem <a.abuhazeem@gmail.com>
2018-03-18 19:36:49 +0200

    First Commit

---
+++ BridgeDesign.cs
@@ -0,0 +1,76 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Design_Patterns
+{
+   public interface IRenderer
+    {
+        void RenderShape(string shapeName, string color);
+    }
+
+    public class VectorRenderer : IRenderer
+    {
+        public void RenderShape(string shapeName, string color)
+        {
+            Console.WriteLine($"Vector Renderer to create {shapeName} with {color} color");
+        }
+    }
+
+    public class RasterRenderer : IRenderer
+    {
+        public void RenderShape(string shapeName, string color)
+        {
+            Console.WriteLine($"Raster Renderer to create {shapeName} with {color} color");
+        }
+    }
+
+    public abstract class Shape
+    {
+        protected IRenderer Renderer { get; set; }
+        public string Color { get; set; }
+
+        public Shape(IRenderer renderer)
+        {
+            this.Renderer = renderer;
+        }
+
+        public abstract void Draw();    
+
+    }
+
+    public class Line : Shape
+    {
+        public Line(IRenderer renderer, string color): base(renderer)
+        {
+            this.Color = color;
+        }
+        public override void Draw()
+        {
+            Renderer.RenderShape(this.GetType().Name, Color);
+        }
+    }
+
+    public class Demo
+        {
+            static void Main(string[] args)
+            {
+
+                IRenderer renderer = new VectorRenderer();
+            Shape line = new Line(renderer, "Red");
+
+            line.Draw();
+
+
+
+
+
+           
+        }
+        }
+ }
+
